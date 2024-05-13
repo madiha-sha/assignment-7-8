de756cb2c4dd5debbe168d36a59b7c1136a5bf4e
Madiha shah <162611745+madiha-sha@users.noreply.github.com>
2024-05-13 08:37:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//assignment 7
+//add the number
+var AdditionRusult = 4 + 4;
+console.log("additionRusult:", AdditionRusult);
+//subtract the number
+var subtractResult = 10 - 2;
+console.log("subtractResult:", subtractResult);
+//multiplication
+var multiplicationRusult = 4 * 2;
+console.log("multiplicationResult:", multiplicationRusult);
+//devide
+var devideResult = 16 / 2;
+console.log("devideResult:", devideResult);
+//assignment 8
+console.log(4 + 4);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
---
+++ main.ts
@@ -0,0 +1,26 @@
+//assignment 7
+//add the number
+let AdditionRusult : number = 4+4;
+console.log("additionRusult:", AdditionRusult);
+
+//subtract the number
+let subtractResult : number = 10-2;
+console.log("subtractResult:", subtractResult);
+
+//multiplication
+let multiplicationRusult : number = 4*2;
+console.log("multiplicationResult:", multiplicationRusult);
+
+//devide
+let devideResult : number = 16/2;
+console.log("devideResult:", devideResult);
+
+//assignment 8
+console.log(4+4);
+console.log(10-2);
+console.log(4*2);
+console.log(16/2);
+
+
+
+
